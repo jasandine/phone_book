7871be73e3b160c9570ac40d4123954a0bdf9644
Joey <Joedawn@Joey-2.local>
2015-01-16 14:00:51 -0800

    initial specs have passed

--- lib/contact.rb
+++ lib/contact.rb
@@ -4,9 +4,34 @@ class Contact
   define_method(:initialize) do |home, cell, work|
   	@home = home
   	@cell = cell
-  	@year = year
+  	@work = work
+  	@id = @@contacts.length().+(1)
   end
 
   define_method(:home) do 
   	@home
   end
+
+  define_method(:cell) do
+  	@cell
+  end
+
+  define_method(:work) do
+  	@work
+  end
+
+  define_singleton_method(:all) do
+  	@@contacts
+  end
+
+  define_method(:save) do
+  	@@contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+  	@@contacts = []
+  end
+
+
+
+end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -7,9 +7,51 @@ describe(Contact) do
 end
 
   describe("#home") do 
-  	it("returns the home phone number")
-  	test_contact = Contact.new(5413901111, 5413902222, 5413903333)
-  	test_contact.save()
-  	expect(test_contact.home()).to(eq(5413901111))
+  	it("returns the home phone number") do 
+  	  test_contact = Contact.new(5413901111, 5413902222, 5413903333)
+  	  test_contact.save()
+  	  expect(test_contact.home()).to(eq(5413901111))
   end
 end
+
+  describe("#cell") do 
+  	it("returns the cell phone number") do 
+  		test_contact = Contact.new(5413901111, 5413902222, 5413903333)
+  		test_contact.save()
+  		expect(test_contact.cell()).to(eq(5413902222))
+  	end
+  end
+
+  describe("#work") do 
+	  it("returns the work phone number") do 
+	  	test_contact = Contact.new(5413901111, 5413902222, 5413903333)
+  		test_contact.save()
+  		expect(test_contact.work()).to(eq(5413903333))
+  	end
+  end
+
+  describe("#save") do 
+  	it ("adds a name to the array of saved numbers") do 
+  		test_contact = Contact.new(5413901111, 5413902222, 5413903333)
+  		test_contact.save()
+  		expect(Contact.all()).to(eq([test_contact]))
+  	end
+  end
+
+  describe(".all") do 
+  	it("is empty at first") do 
+  		expect(Contact.all()).to(eq([]))
+  	end
+  end
+
+  describe(".clear") do
+  	it("empties out all of the saved contacts") do 
+  		Contact.new(5413901111, 5413902222, 5413903333).save()
+  		Contact.clear()
+  		expect(Contact.all()).to(eq([]))
+  	end
+  end
+end
+
+
+
