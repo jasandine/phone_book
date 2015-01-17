f81052f33bd73e6b29df64b7e85213755a9d885a
Joey <Joedawn@Joey-2.local>
2015-01-16 16:35:46 -0800

    sinatra is working and app is functional

--- app.rb
+++ app.rb
@@ -2,20 +2,29 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/contact')
+require('./lib/phone')
 
 get ('/') do 
-	@contacts = Contact.all()
+	@phones = Phone.all()
 	erb(:index)
 end
 
-post('/contact') do 
+post('/phones') do 
+	name = params.fetch('name')
+	Phone.new(name).save()
+	@phones = Phone.all()
+	erb(:index)
+end
+
+post('/contacts') do 
 	home = params.fetch("home") 
 	cell = params.fetch("cell")
 	work = params.fetch("work")
-	contact = Contact.new(home, cell, work)
-	contact.save()
-	@contacts = Contact.all()
-	erb(:index)
+	@contact = Contact.new(home, cell, work)
+	@contact.save()
+  @phone = Phone.find(params.fetch('phone_id').to_i())
+  @phone.add_contact(@contact)
+  erb(:phone)
 end
 
 get('/contacts/:id') do 
@@ -23,3 +32,7 @@ get('/contacts/:id') do
 	erb(:contact)
 end
 
+get('/phones/:id') do 
+	@phone = Phone.find(params.fetch('id').to_i())
+	erb(:phone)
+end
